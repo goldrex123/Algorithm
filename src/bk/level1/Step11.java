22a5902867a828328b74183e22c36705bc5f64b3
kimjunyoung <goldrex123@naver.com>
2023-08-24 12:36:11 +0900

    11382 문제

---
+++ src/bk/level1/Step11.java
@@ -0,0 +1,17 @@
+package bk.level1;
+// 문제번호 11382 : 꼬마 정민
+
+import java.util.Scanner;
+
+public class Step11 {
+    public static void main(String[] args) {
+        long a,b,c;
+
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextLong();
+        b = scan.nextLong();
+        c = scan.nextLong();
+
+        System.out.println(a+b+c);
+    }
+}
