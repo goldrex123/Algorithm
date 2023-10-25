aa2382f7201784d3ecf6f3e68019f9ffde96dd2e
kimjunyoung <goldrex123@naver.com>
2023-10-25 14:15:31 +0900

    24263 문제 해결

--- src/bk/level11/Step2.java
+++ src/bk/level11/Step2.java
@@ -1,7 +1,17 @@
 package bk.level11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //문제번호 24263 : 알고리즘 수업 - 알고리즘의 수행시간 2
 public class Step2 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n);
+		System.out.println(1);
 	}
 }
