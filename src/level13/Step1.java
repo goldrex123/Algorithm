bf69415dff64d78b7f329d721b5b93b16f84304e
kimjunyoung <goldrex123@naver.com>
2023-11-11 16:17:50 +0900

    2750 문제

---
+++ src/level13/Step1.java
@@ -0,0 +1,25 @@
+package level13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+// 문제 번호 2750 : 수 정렬하기
+public class Step1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		List<Integer> numbers = new ArrayList<Integer>();
+		
+		for(int i = 0; i < N; i++ ) {
+			numbers.add(Integer.parseInt(br.readLine()));
+		}
+		
+		numbers.stream().sorted().forEach(x -> {
+			System.out.println(x);
+		});
+	}
+}
