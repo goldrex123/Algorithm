aeb05c4e6a6f18e8aaecfe85e2e5cd82934ab8f9
kimjunyoung <goldrex123@naver.com>
2023-12-09 15:28:29 +0900

    14425 문제

---
+++ src/bk/level14/Step2.java
@@ -0,0 +1,36 @@
+package bk.level14;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// 문제번호 14425 : 문자열 집합
+public class Step2 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer tokent = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(tokent.nextToken());
+		int M = Integer.parseInt(tokent.nextToken());
+		int strCnt = 0;
+		
+		Map<String,Integer> words = new HashMap<String,Integer>();
+		
+		for(int i = 0; i < N; i ++) {
+			words.put(br.readLine(), 0);
+		}
+		
+		for(int i = 0; i < M; i++) {
+			if(words.containsKey(br.readLine())) {
+				strCnt++;
+			}
+		}
+		
+		System.out.println(strCnt);
+		
+	}
+
+}
