e1e030e561f24c5852443307122e3f0fe583cff1
kimjunyoung <goldrex123@naver.com>
2024-01-31 23:52:43 +0900

    24723 문제

---
+++ src/bk/level19/Step2.java
@@ -0,0 +1,16 @@
+package bk.level19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//문제번호 24723 : 녹색거탑
+public class Step2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println((int)Math.pow(2,N));
+    }
+}
