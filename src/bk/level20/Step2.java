4543e824bca175616827e4241f7d9e05cdf4703b
kimjunyoung <goldrex123@naver.com>
2024-02-07 13:49:04 +0900

    map 로직 수정

--- src/bk/level20/Step2.java
+++ src/bk/level20/Step2.java
@@ -7,13 +7,13 @@ import java.util.HashMap;
 
 // 문제번호 25192 : 인사성 밝은 곰곰이
 public class Step2 {
+    public static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         HashMap<String, Integer> map = new HashMap<>();
 
-        int cnt = 0;
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             if(str.equals("ENTER")) {
@@ -21,12 +21,11 @@ public class Step2 {
                 continue;
             }
 
-            if (map.containsKey(str)) {
-                continue;
-            }
-            map.put(str, 1);
-            cnt++;
+           map.computeIfAbsent(str, key -> {cnt++;
+              return 1;
+           });
         }
+        
         System.out.println(cnt);
     }
 }
