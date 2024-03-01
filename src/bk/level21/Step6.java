269c5cea10b13c027f42a46e0dea4608eb241de6
kimjunyoung <goldrex123@naver.com>
2024-03-01 13:22:32 +0900

    2447 문제

--- src/bk/level21/Step6.java
+++ src/bk/level21/Step6.java
@@ -6,40 +6,48 @@ import java.io.InputStreamReader;
 
 //문제번호 2447 : 별 찍기 - 10
 public class Step6 {
-    public static StringBuilder sb = new StringBuilder();
+    public static char[][] star;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        star = new char[N][N];
 
-        printStar(N, false);
+        printStar(0,0, N,false);
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(star[i][j]);
+            }
+            sb.append("\n");
+        }
         System.out.println(sb);
     }
 
-    public static void printStar(int N, boolean blank) {
+    public static void printStar(int x, int y, int N, boolean blank) {
         if (blank) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    sb.append(" ");
+            for (int i = x; i < x+N; i++) {
+                for (int j = y; j < y+N; j++) {
+                    star[i][j] = ' ';
                 }
             }
             return;
         }
 
         if (N == 1) {
-            sb.append("*");
+            star[x][y] = '*';
             return;
         }
 
         int size = N / 3;
         int count = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int i = x; i < x+N; i+=size) {
+            for (int j = y; j < y+N; j+=size) {
                 count++;
-                printStar(size, count == 5);
+                printStar(i,j,size, count == 5);
             }
-            sb.append("\n");
         }
 
     }
