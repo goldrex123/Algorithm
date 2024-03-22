b743fc3b53ba71843bd940818b2c95b2d87fe0d4
kimjunyoung <goldrex123@naver.com>
2024-03-22 23:42:38 +0900

    15651 문제

---
+++ src/bk/level22/Step3.java
@@ -0,0 +1,43 @@
+package bk.level22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 문제번호 15651 : N과 M(3)
+public class Step3 {
+
+    public static int N,M;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        arr = new int[M];
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if(depth == M) {
+            for (int value : arr) {
+                sb.append(value).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+                arr[depth] = i + 1;
+                dfs(depth+1);
+        }
+
+    }
+}
