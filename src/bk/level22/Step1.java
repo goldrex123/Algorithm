1324b51e940393316126a5ea04df5f9050751347
kimjunyoung <goldrex123@naver.com>
2024-07-08 22:29:35 +0900

    15649 다시 풀이

--- src/bk/level22/Step1.java
+++ src/bk/level22/Step1.java
@@ -8,29 +8,32 @@ import java.util.StringTokenizer;
 // 문제번호 15649 : N과 M(1)
 public class Step1 {
 
-    public static int N,M;
-    public static boolean[] visit;
-    public static int[] arr;
-    public static StringBuilder sb = new StringBuilder();
+    static int N,M;
+
+    static boolean[] visit;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer token = new StringTokenizer(br.readLine());
+        StringTokenizer tokenizer = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(token.nextToken());
-        M = Integer.parseInt(token.nextToken());
+        N = Integer.parseInt(tokenizer.nextToken());
+        M = Integer.parseInt(tokenizer.nextToken());
 
-        arr = new int[M];
+
         visit = new boolean[N];
+        arr = new int[M];
         dfs(0);
 
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 
     public static void dfs(int depth) {
-        if(depth == M) {
-            for (int value : arr) {
-                sb.append(value).append(" ");
+        if (depth == M) {
+            for (int i = 0; i < arr.length; i++) {
+                sb.append(arr[i]).append(" ");
             }
             sb.append("\n");
             return;
@@ -39,12 +42,11 @@ public class Step1 {
         for (int i = 0; i < N; i++) {
             if(!visit[i]) {
                 visit[i] = true;
-                arr[depth] = i + 1;
-                dfs(depth+1);
+                arr[depth] = i+1;
+                dfs(depth + 1);
 
                 visit[i] = false;
             }
         }
-
     }
 }
